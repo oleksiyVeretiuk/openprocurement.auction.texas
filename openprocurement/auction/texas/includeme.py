e7b4978bf1de132edb800250a6b884b4a729f746
Oleksiy Veretiuk <oleksiy.veretiuk@quintagroup.org>
2018-09-12 15:50:40 +0300

    Add entry_point to include proxy url

--- openprocurement/auction/texas/includeme.py
+++ openprocurement/auction/texas/includeme.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 from openprocurement.auction.includeme import _register
+from openprocurement.auction.auctions_server import auctions_proxy
+from openprocurement.auction.interfaces import IAuctionsServer
 
 
 def texasProcedure(components, procurement_method_types):
     for procurementMethodType in procurement_method_types:
         _register(components, procurementMethodType)
+
+
+def texas_routes(components):
+    server = components.queryUtility(IAuctionsServer)
+    server.add_url_rule('/texas-auctions/<auction_doc_id>/<path:path>', 'auctions',
+                     auctions_proxy,
+                     methods=['GET', 'POST'])
